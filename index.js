d8b29191d29e91f2eafba50fc73cb1b8ee15f7d1
David Felix <david.felix02@hotmail.com>
2020-09-18 03:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function findMatching(drivers,driver) {
 }
 
 function fuzzyMatch (drivers,driver) {
-  return drivers.filter(person => person.name === driver || person.name[0] === driver[0])
+  return drivers.filter(person => {
+    if (typeof person !== "undefined" || typeof driver !== "undefined"){
+      if (person.name === driver || person.name[0] === driver[0]){
+        return driver;
+      }
+    }
+  });
+    
 }
